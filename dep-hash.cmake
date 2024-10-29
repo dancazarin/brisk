b3f6ccd9a1c8de97829a1a5fe47a8552db734153
Brisk <brisk@brisklib.com>
2024-10-29 14:34:30 +0000

    Write README.md on install

--- dep-hash.cmake
+++ dep-hash.cmake
@@ -7,17 +7,25 @@ file(GLOB_RECURSE FILES "${CMAKE_CURRENT_LIST_DIR}/cmake/ports/*" "${CMAKE_CURRE
 
 set(HASHES)
 
-message("-- Hashing files")
+if (NOT DEFINED DEP_HASH_SILENT)
+    message("-- Hashing files")
+endif ()
 
 foreach (FILE IN LISTS FILES)
     file(READ ${FILE} CONTENT)
     string(SHA256 HASH "${CONTENT}")
     file(RELATIVE_PATH REL_FILE ${CMAKE_CURRENT_LIST_DIR} ${FILE})
-    message("        ${REL_FILE} -> ${HASH}")
+    if (NOT DEFINED DEP_HASH_SILENT)
+        message("        ${REL_FILE} -> ${HASH}")
+    endif ()
     list(APPEND HASHES "${HASH}")
 endforeach ()
 
-message("-- Combined hash is")
+if (NOT DEFINED DEP_HASH_SILENT)
+    message("-- Combined hash is")
+endif ()
 
 string(SHA256 DEP_HASH "${HASHES}")
-message(STATUS ${DEP_HASH})
+if (NOT DEFINED DEP_HASH_SILENT)
+    message(STATUS ${DEP_HASH})
+endif ()
